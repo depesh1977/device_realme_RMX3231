65ecbf63eec9381cae1f2ef744c603ef17e88209
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2023-12-29 02:44:55 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-LOCAL_PATH := device/realme/RMX3231
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RMX3231.mk
 
-COMMON_LUNCH_CHOICES := twrp__RMX3231-eng
+COMMON_LUNCH_CHOICES := twrp_RMX3231-eng
